8633d129754f61009895e1fcf08bd7e35994886b
WanpengXu <xwp@cumt.edu.cn>
2024-01-15 14:43:30 +0800

    Full Commit

---
+++ web_demo_app.py
@@ -0,0 +1,117 @@
+from langchain.vectorstores import Chroma
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+import os
+from LangChain_LLM import InternLM
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+
+def load_chain():
+    # 加载问答链
+    # 定义 Embeddings
+    embeddings = HuggingFaceEmbeddings(model_name="/home/xlab-app-center/model/sentence-transformer")
+
+    # 向量数据库持久化路径
+    # persist_directory = 'data_base/vector_db/chroma'
+    persist_directory = 'data_base/vector_db/chroma_psychology'
+
+    # 加载数据库
+    vectordb = Chroma(
+        persist_directory=persist_directory,  # 允许我们将persist_directory目录保存到磁盘上
+        embedding_function=embeddings
+    )
+
+    # 加载自定义 LLM
+    llm = InternLM(model_path = "/home/xlab-app-center/model/InternLM-chat-7b")
+
+    # 定义一个 Prompt Template
+    template = """你是一个心理学专家，你需要使用以下上下文来回答最后的问题。如果你不知道答案，就说你不知道，尽量使答案简明扼要。总是使用中文回答。
+    问题: {question}
+    可参考的上下文：
+    ...
+    {context}
+    ...
+    如果给定的上下文无法让你做出回答，请回答你不知道。
+    有用的回答:"""
+
+
+    QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context","question"],template=template)
+
+    # 运行 chain
+    qa_chain = RetrievalQA.from_chain_type(llm,retriever=vectordb.as_retriever(),return_source_documents=True,chain_type_kwargs={"prompt":QA_CHAIN_PROMPT})
+    
+    return qa_chain
+
+class Model_Center():
+    """
+    存储检索问答链的对象 
+    """
+    def __init__(self):
+        # 构造函数，加载检索问答链
+        self.chain = load_chain()
+
+    def qa_chain_self_answer(self, question: str, chat_history: list = []):
+        """
+        调用问答链进行回答
+        """
+        if question == None or len(question) < 1:
+            return "", chat_history
+        try:
+            chat_history.append(
+                (question, self.chain({"query": question})["result"]))
+            # 将问答结果直接附加到问答历史中，Gradio 会将其展示出来
+            return "", chat_history
+        except Exception as e:
+            return e, chat_history 
+
+
+# OpenXLab 环境配置
+import os
+# 设置环境变量
+os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+# 下载模型
+os.system('huggingface-cli download --resume-download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 --local-dir /home/xlab-app-center/model/sentence-transformer')
+# 将模型导入
+from openxlab.model import download
+download(model_repo='OpenLMLab/InternLM-chat-7b', output='/home/xlab-app-center/model/InternLM-chat-7b')
+
+
+import gradio as gr
+
+# 实例化核心功能对象
+model_center = Model_Center()
+# 创建一个 Web 界面
+block = gr.Blocks()
+with block as demo:
+    with gr.Row(equal_height=True):   
+        with gr.Column(scale=15):
+            # 展示的页面标题
+            gr.Markdown("""<h1><center>InternLM</center></h1>
+                <center>书生浦语</center>
+                """)
+
+    with gr.Row():
+        with gr.Column(scale=4):
+            # 创建一个聊天机器人对象
+            chatbot = gr.Chatbot(height=450, show_copy_button=True)
+            # 创建一个文本框组件，用于输入 prompt。
+            msg = gr.Textbox(label="Prompt/问题")
+
+            with gr.Row():
+                # 创建提交按钮。
+                db_wo_his_btn = gr.Button("Chat")
+            with gr.Row():
+                # 创建一个清除按钮，用于清除聊天机器人组件的内容。
+                clear = gr.ClearButton(
+                    components=[chatbot], value="Clear console")
+                
+        # 设置按钮的点击事件。当点击时，调用上面定义的 qa_chain_self_answer 函数，并传入用户的消息和聊天历史记录，然后更新文本框和聊天机器人组件。
+        db_wo_his_btn.click(model_center.qa_chain_self_answer, inputs=[
+                            msg, chatbot], outputs=[msg, chatbot])
+
+    gr.Markdown("""提醒：<br>
+    1. 初始化数据库时间可能较长，请耐心等待。
+    2. 使用中如果出现异常，将会在文本输入框进行展示，请不要惊慌。 <br>
+    """)
+gr.close_all()
+# 直接启动
+demo.launch()
